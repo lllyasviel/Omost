ea8d47554f4865ed4ea6cdd836f3db67d1ef2833
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-29 09:16:23 -0700

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -6,6 +6,7 @@ HF_TOKEN = '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'  # Remember to invalid this t
 import lib_omost.memory_management as memory_management
 
 import torch
+import numpy as np
 import gradio as gr
 
 from threading import Thread
@@ -14,6 +15,7 @@ from threading import Thread
 from transformers.models.phi3.modeling_phi3 import Phi3PreTrainedModel
 Phi3PreTrainedModel._supports_sdpa = True
 
+from PIL import Image
 from transformers import AutoModelForCausalLM, AutoTokenizer, TextIteratorStreamer
 from diffusers import AutoencoderKL, UNet2DConditionModel
 from diffusers.models.attention_processor import AttnProcessor2_0
@@ -82,6 +84,128 @@ llm_tokenizer = AutoTokenizer.from_pretrained(
 )
 
 memory_management.unload_all_models(llm_model)
+
+
+@torch.inference_mode()
+def pytorch2numpy(imgs):
+    results = []
+    for x in imgs:
+        y = x.movedim(0, -1)
+        y = y * 127.5 + 127.5
+        y = y.detach().float().cpu().numpy().clip(0, 255).astype(np.uint8)
+        results.append(y)
+    return results
+
+
+@torch.inference_mode()
+def numpy2pytorch(imgs):
+    h = torch.from_numpy(np.stack(imgs, axis=0)).float() / 127.5 - 1.0
+    h = h.movedim(-1, 1)
+    return h
+
+
+def resize_without_crop(image, target_width, target_height):
+    pil_image = Image.fromarray(image)
+    resized_image = pil_image.resize((target_width, target_height), Image.LANCZOS)
+    return np.array(resized_image)
+
+
+with torch.inference_mode():
+    example_response = open('./example_outputs.md', 'rt', encoding='utf-8').read()
+    example_response = '```python\n' + example_response + '\n```'
+
+    canvas = omost_canvas.Canvas.from_bot_response(example_response)
+    canvas_outputs = canvas.process()
+
+    guidance_scale = 7.0
+    hr_fix = False
+    use_initial_latent = False
+    H, W = 1024, 1024
+
+    rng = torch.Generator(device=memory_management.gpu).manual_seed(98654)
+
+    memory_management.load_models_to_gpu([text_encoder, text_encoder_2])
+
+    positive_cond, positive_pooler, negative_cond, negative_pooler = pipeline.all_conds_from_canvas(canvas_outputs, default_negative)
+
+    if use_initial_latent:
+        memory_management.load_models_to_gpu([vae])
+        initial_latent = torch.from_numpy(canvas_outputs['initial_latent'])[None].movedim(-1, 1) / 127.5 - 1.0
+        initial_latent_blur = 40
+        initial_latent = torch.nn.functional.avg_pool2d(
+            torch.nn.functional.pad(initial_latent, (initial_latent_blur, ) * 4, mode='reflect'),
+            kernel_size=(initial_latent_blur * 2 + 1, ) * 2, stride=(1, 1))
+        initial_latent = torch.nn.functional.interpolate(initial_latent, (H, W))
+        initial_latent = initial_latent.to(dtype=vae.dtype, device=vae.device)
+        initial_latent = vae.encode(initial_latent).latent_dist.mode() * vae.config.scaling_factor
+    else:
+        initial_latent = torch.zeros(size=(1, 4, H // 8, W // 8), dtype=torch.float32)
+
+    memory_management.load_models_to_gpu([unet])
+
+    initial_latent = initial_latent.to(dtype=unet.dtype, device=unet.device)
+
+    latents = pipeline(
+        initial_latent=initial_latent,
+        strength=1.0,
+        num_inference_steps=25,
+        batch_size=1,
+        prompt_embeds=positive_cond,
+        negative_prompt_embeds=negative_cond,
+        pooled_prompt_embeds=positive_pooler,
+        negative_pooled_prompt_embeds=negative_pooler,
+        generator=rng,
+        guidance_scale=guidance_scale,
+    ).images
+
+    memory_management.load_models_to_gpu([vae])
+    latents = latents.to(dtype=vae.dtype, device=vae.device) / vae.config.scaling_factor
+    pixels = vae.decode(latents).sample
+    B, C, H, W = pixels.shape
+    pixels = pytorch2numpy(pixels)
+
+    if hr_fix:
+        highres_scale = 1.5
+        highres_denoise = 0.35
+        highres_steps_diffusers = 10
+
+        pixels = [
+            resize_without_crop(
+                image=p,
+                target_width=int(round(W * highres_scale / 64.0) * 64),
+                target_height=int(round(H * highres_scale / 64.0) * 64)
+            ) for p in pixels
+        ]
+
+        pixels = numpy2pytorch(pixels).to(device=vae.device, dtype=vae.dtype)
+        latents = vae.encode(pixels).latent_dist.mode() * vae.config.scaling_factor
+
+        memory_management.load_models_to_gpu([unet])
+        latents = latents.to(device=unet.device, dtype=unet.dtype)
+
+        latents = pipeline(
+            initial_latent=latents,
+            strength=highres_denoise,
+            num_inference_steps=highres_steps_diffusers,
+            batch_size=1,
+            prompt_embeds=positive_cond,
+            negative_prompt_embeds=negative_cond,
+            pooled_prompt_embeds=positive_pooler,
+            negative_pooled_prompt_embeds=negative_pooler,
+            generator=rng,
+            guidance_scale=guidance_scale,
+        ).images
+
+        memory_management.load_models_to_gpu([vae])
+        latents = latents.to(dtype=vae.dtype, device=vae.device) / vae.config.scaling_factor
+        pixels = vae.decode(latents).sample
+        pixels = pytorch2numpy(pixels)
+
+    for i in range(len(pixels)):
+        image = Image.fromarray(pixels[i])
+        image.save(f"output_{i}.png")
+
+    exit(0)
 
 
 def chat_fn(message: str, history: list, temperature: float, top_p: float, max_new_tokens: int) -> str:
