2a57a6546a7b2c66d1b6dac1c06af457d03fb1d4
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-29 08:40:37 -0700

    Update memory_management.py

--- lib_omost/memory_management.py
+++ lib_omost/memory_management.py
@@ -2,6 +2,7 @@
 # wHy sO seRiOuS?
 
 import torch
+from contextlib import contextmanager
 
 
 high_vram = False
@@ -9,6 +10,20 @@ gpu = torch.device('cuda')
 cpu = torch.device('cpu')
 
 models_in_gpu = []
+
+
+@contextmanager
+def movable_bnb_model(m):
+    if hasattr(m, 'quantization_method'):
+        m.quantization_method_backup = m.quantization_method
+        del m.quantization_method
+    try:
+        yield None
+    finally:
+        if hasattr(m, 'quantization_method_backup'):
+            m.quantization_method = m.quantization_method_backup
+            del m.quantization_method_backup
+    return
 
 
 def load_models_to_gpu(models):
@@ -19,28 +34,19 @@ def load_models_to_gpu(models):
 
     models_to_load = [m for m in set(models) if m not in models_in_gpu]
 
-    for m in models_to_load:
-        if hasattr(m, 'quantization_method'):
-            m.quantization_method_backup = m.quantization_method
-            del m.quantization_method
-
     if not high_vram:
         for m in models_in_gpu:
-            m.to(cpu)
+            with movable_bnb_model(m):
+                m.to(cpu)
             print('Unload to CPU:', m.__class__.__name__)
         models_in_gpu = []
 
     for m in models_to_load:
-        m.to(gpu)
+        with movable_bnb_model(m):
+            m.to(gpu)
         print('Load to GPU:', m.__class__.__name__)
 
     models_in_gpu = list(set(models_in_gpu + models))
-
-    for m in models_to_load:
-        if hasattr(m, 'quantization_method_backup'):
-            m.quantization_method = m.quantization_method_backup
-            del m.quantization_method_backup
-
     torch.cuda.empty_cache()
     return
 
