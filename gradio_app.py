aea29fb896f0432fcb79ce4be42f1d700a8cb9af
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-29 23:05:49 -0700

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -318,7 +318,7 @@ with gr.Blocks(fill_height=True, css=css) as demo:
             examples = gr.Dataset(
                 samples=[
                     ['generate an image of the fierce battle of warriors and dragons'],
-                    ['burn everything with evil, impure, dark, immortal ghostly fire']
+                    ['burn everything in this image with evil, impure, dark, immortal ghostly fire']
                 ],
                 components=[gr.Textbox(visible=False)],
                 label='Quick Prompts'
