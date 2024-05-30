a533e51e5b7f78d61fc69ef5c5459a093683b51d
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-05-29 20:08:29 -0700

    Update gradio_app.py

--- gradio_app.py
+++ gradio_app.py
@@ -255,7 +255,7 @@ code {white-space: pre-wrap !important;}
 .gradio-container {max-width: none !important;}
 .outer_parent {flex: 1;}
 .inner_parent {flex: 1;}
-footer {display: none;}
+footer {display: none !important; visibility: hidden !important;}
 '''
 
 with gr.Blocks(fill_height=True, css=css) as demo:
